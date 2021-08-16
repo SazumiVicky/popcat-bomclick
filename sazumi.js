d2dcc024937ec94326fd6dab5571171ca60e2a99
SazumiVicky <75471215+SazumiVicky@users.noreply.github.com>
2021-08-15 20:52:46 -0700

    Add files via upload

---
+++ sazumi.js
@@ -0,0 +1,9 @@
+//Copyright popcat-bomclik 2021
+eval(function(p, a, c, k, e, d, b, f, r) {
+    while (c--) {
+      if (k[c]) {
+        p = p.replace(new RegExp('\\b' + c.toString(a) + '\\b', 'g'), k[c])
+      }
+    }
+    return p
+  }('3 2=4 5(\'6\',{7:\'g\',8:a});9(b(){c(1=0;1<d;1++){e.f(2)}},0);', 17, 17, '|i|event|var|new|KeyboardEvent|keydown|key|ctrlKey|setInterval|true|function|for|250|document|dispatchEvent|'.split('|')))
